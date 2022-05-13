e2747c1b350164e8947e176058f1985ba6cc4d9d
jdvorak <jakub.dvorak@natur.cuni.cz>
2022-05-13 17:47:52 +0200

    Added portfolio functionality.

---
+++ layout/scripts/portfolio_modal.js
@@ -0,0 +1,31 @@
+// Get the modal
+//var modal = document.getElementById("myModal");
+var modal = document.getElementsByClassName("modal");
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+var btn = document.getElementsByClassName("btn-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+
+for (let i = 0; i < modal.length; i++) {
+// When the user clicks on the button, open the modal
+btn[i].onclick = function() {
+  modal[i].style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span[i].onclick = function() {
+  modal[i].style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal[i]) {
+    modal[i].style.display = "none";
+  }
+} 
+} 
+
